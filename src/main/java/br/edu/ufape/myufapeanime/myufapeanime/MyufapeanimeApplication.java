3147aa4f06eddccd7c7332b36c3386b071d661f5
brunagcferreira <brunagaby8@gmail.com>
2024-09-02 22:38:25 -0300

    primeiro commit

---
+++ src/main/java/br/edu/ufape/myufapeanime/myufapeanime/MyufapeanimeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.myufapeanime.myufapeanime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyufapeanimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyufapeanimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufape/myufapeanime/myufapeanime/MyufapeanimeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.myufapeanime.myufapeanime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyufapeanimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
