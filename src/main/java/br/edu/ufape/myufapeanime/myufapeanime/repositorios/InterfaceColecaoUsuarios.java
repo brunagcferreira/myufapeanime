7f6a552491f5a2c62f55823744360f1c80b20088
brunagcferreira <brunagaby8@gmail.com>
2024-09-02 23:17:29 -0300

    criacao das classes basicas

---
+++ src/main/java/br/edu/ufape/myufapeanime/myufapeanime/repositorios/InterfaceColecaoUsuarios.java
@@ -0,0 +1,11 @@
+package br.edu.ufape.myufapeanime.myufapeanime.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.basica.Usuario;
+
+@Repository
+public interface InterfaceColecaoUsuarios extends JpaRepository<Usuario, Long> {
+
+}
