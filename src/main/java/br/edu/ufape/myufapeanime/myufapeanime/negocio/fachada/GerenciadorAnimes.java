9dfdbad0a2b67c4b2d0639538934a1556c3b353f
brunagcferreira <brunagaby8@gmail.com>
2024-09-05 14:54:02 -0300

    add fachada com CadastroUsuario

---
+++ src/main/java/br/edu/ufape/myufapeanime/myufapeanime/negocio/fachada/GerenciadorAnimes.java
@@ -0,0 +1,63 @@
+package br.edu.ufape.myufapeanime.myufapeanime.negocio.fachada;
+
+import java.util.List;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import br.edu.ufape.myufapeanime.myufapeanime.exceptions.UsuarioDuplicadoException;
+import br.edu.ufape.myufapeanime.myufapeanime.exceptions.UsuarioInexistenteException;
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.basica.Anime;
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.basica.Usuario;
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.cadastro.CadastroUsuario;
+
+@Service
+public class GerenciadorAnimes {
+    
+    @Autowired
+    private CadastroUsuario cadastroUsuario;
+
+    /**********IMPLEMENTAÇÃO DE CADASTRO USUARIO ********/
+    //salvar
+    public Usuario saveUsuario(Usuario usuario) throws UsuarioDuplicadoException {
+        return cadastroUsuario.save(usuario);
+    }
+    //atualizar
+    public Usuario updateUsuario(Usuario usuario) throws UsuarioInexistenteException {
+        return cadastroUsuario.update(usuario);
+    }
+    //apagar por id
+    public void deleteUsuarioById(Long id) throws UsuarioInexistenteException {
+        cadastroUsuario.deleteById(id);
+    }
+    //apagar
+    public void deleteUsuario(Usuario usuario) throws UsuarioInexistenteException {
+        cadastroUsuario.delete(usuario);
+    }
+    //listar todos
+    public List<Usuario> findAllUsuarios(){
+        return cadastroUsuario.findAll();
+    }
+    //buscar por id(pode retornar null)
+    public Optional<Usuario> findByIdUsuario(Long id) {
+        return cadastroUsuario.findById(id);
+    }
+    //buscar por nome(pode retornar null)
+    public Optional<Usuario> findByNomeUsuario(String nome) {
+        return cadastroUsuario.findByNome(nome);
+    }
+    //lista assistindo
+    public List<Anime> getAssistindoUsuario(Long usuarioId) throws UsuarioInexistenteException {
+        return cadastroUsuario.getAssistindo(usuarioId);
+    }
+    //lista completos
+    public List<Anime> getCompletosUsuario(Long usuarioId) throws UsuarioInexistenteException {
+        return cadastroUsuario.getCompleto(usuarioId);
+    }
+    //lista quero assistir
+    public List<Anime> getQueroAssistirUsuario(Long usuarioId) throws UsuarioInexistenteException {
+        return cadastroUsuario.getQueroAssistir(usuarioId);
+    }
+
+    
+}
