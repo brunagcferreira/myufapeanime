605b367b71933556b9ce8d9e39b91271584bdbee
Victor Alexandre Saraiva Pimentel <victor_alex2005@hotmail.com>
2024-09-06 16:08:39 -0300

    Fachada de cadastroAnimes no GerenciadorAnimes

--- src/main/java/br/edu/ufape/myufapeanime/myufapeanime/negocio/fachada/GerenciadorAnimes.java
+++ src/main/java/br/edu/ufape/myufapeanime/myufapeanime/negocio/fachada/GerenciadorAnimes.java
@@ -3,6 +3,12 @@ package br.edu.ufape.myufapeanime.myufapeanime.negocio.fachada;
 import java.util.List;
 
 import java.util.Optional;
+
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.cadastro.CadastroAnime;
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.cadastro.cadastroAnimeExceptions.AnimeDuplicadoException;
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.cadastro.cadastroAnimeExceptions.AnimeInexistenteException;
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.cadastro.cadastroAnimeExceptions.NomeDoAnimeVazioException;
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.cadastro.cadastroAnimeExceptions.NumeroDeEpisodiosInvalidoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.edu.ufape.myufapeanime.myufapeanime.negocio.cadastro.cadastroUsuarioExceptions.UsuarioDuplicadoException;
@@ -13,9 +19,12 @@ import br.edu.ufape.myufapeanime.myufapeanime.negocio.cadastro.CadastroUsuario;
 
 @Service
 public class GerenciadorAnimes {
-    
+
     @Autowired
     private CadastroUsuario cadastroUsuario;
+
+    @Autowired
+    private CadastroAnime cadastroAnime;
 
     /**********IMPLEMENTAÇÃO DE CADASTRO USUARIO ********/
     //salvar
@@ -57,5 +66,32 @@ public class GerenciadorAnimes {
     //lista quero assistir
     public List<Anime> getQueroAssistirUsuario(Long usuarioId) throws UsuarioInexistenteException {
         return cadastroUsuario.getQueroAssistir(usuarioId);
-    } 
+    }
+
+    /**********IMPLEMENTAÇÃO DE CADASTRO ANIME ********/
+    //salvar
+    public Anime saveAnime(Anime anime) throws NumeroDeEpisodiosInvalidoException, AnimeDuplicadoException, NomeDoAnimeVazioException {
+        return cadastroAnime.create(anime);
+    }
+    //atualizar
+    public Anime updateAnime(Anime anime) throws AnimeInexistenteException {
+        return cadastroAnime.update(anime);
+    }
+    //apagar por id
+    public void deleteAnimeById(Long id) throws AnimeInexistenteException {
+        cadastroAnime.deleteById(id);
+    }
+    //apagar
+    public void deleteAnime(Anime anime) throws AnimeInexistenteException {
+        cadastroAnime.delete(anime);
+    }
+    //listar todos
+    public List<Anime> findAllAnimes(){
+        return cadastroAnime.findAll();
+    }
+    //buscar por nome(pode retornar null)
+    public List<Anime> findByNomeAnime(String nome) throws AnimeInexistenteException, NomeDoAnimeVazioException {
+        return cadastroAnime.findByNome(nome);
+    }
+
 }
