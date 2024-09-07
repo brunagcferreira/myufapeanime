32131cb6e9138a48bddf02e0ddbeee602208886a
Raylandson Cesário <www.raylandsoncs@gmail.com>
2024-09-07 02:43:47 -0300

    Liberado os requests

--- src/main/java/br/edu/ufape/myufapeanime/myufapeanime/Config/AppConfig.java
+++ src/main/java/br/edu/ufape/myufapeanime/myufapeanime/Config/AppConfig.java
@@ -1,12 +1,17 @@
 package br.edu.ufape.myufapeanime.myufapeanime.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
 
+    @Bean
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
