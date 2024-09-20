0ea3386007ab3789a828b7a4e20e647c161d7b0b
Victor Alexandre Saraiva Pimentel <victor_alex2005@hotmail.com>
2024-09-19 21:15:11 -0300

    Teste de integração Anime

---
+++ src/test/java/br/edu/ufape/myufapeanime/myufapeanime/TestesDeIntegracao/IntegracaoAnimeTest.java
@@ -0,0 +1,48 @@
+package br.edu.ufape.myufapeanime.myufapeanime.TestesDeIntegracao;
+
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.basica.Anime;
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.basica.Usuario;
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.cadastro.cadastroAnimeExceptions.AnimeDuplicadoException;
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.cadastro.cadastroAnimeExceptions.AnimeInexistenteException;
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.cadastro.cadastroAnimeExceptions.NumeroDeEpisodiosInvalidoException;
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.cadastro.cadastroUsuarioExceptions.UsuarioDuplicadoException;
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.cadastro.cadastroUsuarioExceptions.UsuarioInexistenteException;
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.cadastro.cadastroUsuarioExceptions.UsuarioSenhaInvalidaException;
+import br.edu.ufape.myufapeanime.myufapeanime.negocio.fachada.GerenciadorAnimes;
+import br.edu.ufape.myufapeanime.myufapeanime.repositorios.InterfaceRepositorioAnimes;
+import br.edu.ufape.myufapeanime.myufapeanime.repositorios.InterfaceRepositorioUsuarios;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class IntegracaoAnimeTest {
+    @Autowired
+    private GerenciadorAnimes gerenciadorAnimes;
+
+    @Autowired
+    @Qualifier("interfaceRepositorioAnimes")
+    private InterfaceRepositorioAnimes repositorioAnimes;
+
+    @Test
+    public void testCriarEBuscarAnimePorId() throws NumeroDeEpisodiosInvalidoException, AnimeDuplicadoException, AnimeInexistenteException {
+        // Cria um novo anime
+        Anime novoAnime = new Anime();
+        novoAnime.setNome("Pokemon");
+        novoAnime.setNumEpisodios(23);
+        novoAnime.setGenero("Violencia");
+
+        // Salva o anime pela fachada
+        Anime animeSalvo = gerenciadorAnimes.cadastrarAnime(novoAnime);
+
+        // Busca o anime salvo pelo ID e verifica os dados
+        Anime animeEncontrado = gerenciadorAnimes.findByIdAnime(animeSalvo.getId());
+
+        assertEquals(animeSalvo.getNome(), animeEncontrado.getNome());
+        assertEquals(animeSalvo.getNumEpisodios(), animeEncontrado.getNumEpisodios());
+        assertEquals(animeSalvo.getGenero(), animeEncontrado.getGenero());
+    }
+}
